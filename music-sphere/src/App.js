efd67d9298ec03ac9b692e23cbd524a410484332
daniloherreraf <herreraf.danilo@gmail.com>
2023-06-01 17:36:34 -0500

    reducer-token

--- music-sphere/src/App.js
+++ music-sphere/src/App.js
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react'
-import Login from './components/Login'
+import React, { useEffect } from 'react';
+import Login from './components/Login';
+import { useStateProvider } from './utils/StateProvider';
+import { reducerCases } from './utils/Constants';
 
 const App = () => {
+  const [{ token }, dispatch] = useStateProvider();
   useEffect(() => {
-    const hash = window.location.hash
-    if(hash) {
-      const token= hash.substring(1).split("&")[0].split("=")[1]
-      console.log(token)
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split('&')[0].split('=')[1];
+      dispatch({ action: reducerCases.SET_TOKEN, token });
     }
-  }, [])
+  }, [token, dispatch]);
   return (
     <div>
       <Login />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
