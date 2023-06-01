1edcaf408082c10de7b151aa2a5626889fe08133
daniloherreraf <herreraf.danilo@gmail.com>
2023-06-01 17:18:41 -0500

    modificacion-codigo

---
+++ music-sphere/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import Login from './components/Login'
+
+const App = () => {
+  useEffect(() => {
+    const hash = window.location.hash
+    if(hash) {
+      const token= hash.substring(1).split("&")[0].split("=")[1]
+      console.log(token)
+    }
+  }, [])
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default App
