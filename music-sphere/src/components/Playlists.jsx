d41e9137a7cbd8f02f6bc0494522863ad85f51af
daniloherreraf <herreraf.danilo@gmail.com>
2023-06-03 15:44:55 -0500

    playlists

--- music-sphere/src/components/Playlists.jsx
+++ music-sphere/src/components/Playlists.jsx
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { useStateProvider } from '../utils/StateProvider';
 import axios from 'axios';
 import { reducerCases } from '../utils/Constants';
+import styled from 'styled-components';
 
 const Playlists = () => {
-  const [{ token, playlists}, dispatch ] = useStateProvider();
+  const [{ token, playlists }, dispatch] = useStateProvider();
   useEffect(() => {
     const getPlaylistData = async () => {
       const response = await axios.get(
@@ -16,23 +17,55 @@ const Playlists = () => {
           },
         }
       );
-      const {items} = response.data
-     const playlists = items.map(({name, id})=>{
-        return {name,id}
-     })
-     dispatch({type:reducerCases.SET_PLAYLISTS, playlists})
+      const { items } = response.data;
+      const playlists = items.map(({ name, id }) => {
+        return { name, id };
+      });
+      dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
     };
     getPlaylistData();
   }, [token, dispatch]);
-  return <div>
-    {
-        playlists.map(({name,id}) => {
-            return(
-                <li key={id}>{name}</li>
-            )
-        })
-    }
-  </div>;
+  return (
+    <Container>
+      <ul>
+        {playlists.map(({ name, id }) => {
+          return <li key={id}>{name}</li>;
+        })}
+       
+      </ul>
+    </Container>
+  );
 };
 
 export default Playlists;
+
+const Container = styled.div`
+height: 100%;
+overflow: hidden;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 50vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255,255,255,0.6);
+      }
+    }
+    li {
+      display: flex;
+      gap: 1rem;
+      cursor: pointer;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        color: white;
+
+      }
+    }
+  }
+`;
