f56db3dbf3e10934f83679308ebffa563348c8f5
daniloherreraf <herreraf.danilo@gmail.com>
2023-06-07 16:12:21 -0500

    code-playlists

--- music-sphere/src/components/Playlists.jsx
+++ music-sphere/src/components/Playlists.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import { useStateProvider } from '../utils/StateProvider';
-import axios from 'axios';
-import { reducerCases } from '../utils/Constants';
-import styled from 'styled-components';
+import axios from "axios";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { reducerCases } from "../utils/Constants";
+import { useStateProvider } from "../utils/StateProvider";
 
-const Playlists = () => {
+export default function Playlists() {
   const [{ token, playlists }, dispatch] = useStateProvider();
   useEffect(() => {
     const getPlaylistData = async () => {
       const response = await axios.get(
-        'https://api.spotify.com/v1/me/playlists',
+        "https://api.spotify.com/v1/me/playlists",
         {
           headers: {
-            Authorization: 'Bearer ' + token,
-            'Content-Type': 'application/json',
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
           },
         }
       );
@@ -25,46 +25,48 @@ const Playlists = () => {
     };
     getPlaylistData();
   }, [token, dispatch]);
+  const changeCurrentPlaylist = (selectedPlaylistId) => {
+    dispatch({ type: reducerCases.SET_PLAYLIST_ID, selectedPlaylistId });
+  };
   return (
     <Container>
       <ul>
         {playlists.map(({ name, id }) => {
-          return <li key={id}>{name}</li>;
+          return (
+            <li key={id} onClick={() => changeCurrentPlaylist(id)}>
+              {name}
+            </li>
+          );
         })}
-       
       </ul>
     </Container>
   );
-};
-
-export default Playlists;
+}
 
 const Container = styled.div`
-height: 100%;
-overflow: hidden;
+  color: #b3b3b3;
+  height: 100%;
+  overflow: hidden;
   ul {
     list-style-type: none;
     display: flex;
     flex-direction: column;
     gap: 1rem;
     padding: 1rem;
-    height: 50vh;
+    height: 55vh;
     max-height: 100%;
     overflow: auto;
     &::-webkit-scrollbar {
       width: 0.7rem;
       &-thumb {
-        background-color: rgba(255,255,255,0.6);
+        background-color: rgba(255, 255, 255, 0.6);
       }
     }
     li {
-      display: flex;
-      gap: 1rem;
-      cursor: pointer;
       transition: 0.3s ease-in-out;
+      cursor: pointer;
       &:hover {
         color: white;
-
       }
     }
   }
