8643d306dc3fb5a7daef11ca4d0c7f222a1b0990
daniloherreraf <herreraf.danilo@gmail.com>
2023-06-07 16:13:00 -0500

    code-finish

--- music-sphere/src/components/Spotify.jsx
+++ music-sphere/src/components/Spotify.jsx
@@ -1,63 +1,75 @@
-import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
-import Sidebar from './Sidebar';
-import Body from './Body';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import { useStateProvider } from '../utils/StateProvider';
-import axios from 'axios';
-import { reducerCases } from '../utils/Constants';
+import React, { useEffect, useRef, useState } from "react";
+import Sidebar from "./Sidebar";
+import styled from "styled-components";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import axios from "axios";
+import { useStateProvider } from "../utils/StateProvider";
+import Body from "./Body";
+import { reducerCases } from "../utils/Constants";
 
-const Spotify = () => {
+export default function Spotify() {
   const [{ token }, dispatch] = useStateProvider();
-  const bodyRef = useRef();
   const [navBackground, setNavBackground] = useState(false);
   const [headerBackground, setHeaderBackground] = useState(false);
+  const bodyRef = useRef();
   const bodyScrolled = () => {
     bodyRef.current.scrollTop >= 30
       ? setNavBackground(true)
       : setNavBackground(false);
-      bodyRef.current.scrollTop >= 268
+    bodyRef.current.scrollTop >= 268
       ? setHeaderBackground(true)
       : setHeaderBackground(false);
   };
-
   useEffect(() => {
     const getUserInfo = async () => {
-      const { data } = await axios.get('https://api.spotify.com/v1/me', {
+      const { data } = await axios.get("https://api.spotify.com/v1/me", {
         headers: {
-          Authorization: 'Bearer ' + token,
-          'Content-Type': 'application/json',
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
         },
       });
       const userInfo = {
         userId: data.id,
-        userName: data.display_name,
+        userUrl: data.external_urls.spotify,
+        name: data.display_name,
       };
-      console.log(userInfo);
       dispatch({ type: reducerCases.SET_USER, userInfo });
     };
     getUserInfo();
   }, [dispatch, token]);
+  useEffect(() => {
+    const getPlaybackState = async () => {
+      const { data } = await axios.get("https://api.spotify.com/v1/me/player", {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+      dispatch({
+        type: reducerCases.SET_PLAYER_STATE,
+        playerState: data.is_playing,
+      });
+    };
+    getPlaybackState();
+  }, [dispatch, token]);
   return (
     <Container>
-      <div className="spotify_body">
+      <div className="spotify__body">
         <Sidebar />
         <div className="body" ref={bodyRef} onScroll={bodyScrolled}>
-          <Navbar navBackground={navBackground}/>
-          <div className="body_contents">
-            <Body headerBackground={headerBackground}/>
+          <Navbar navBackground={navBackground} />
+          <div className="body__contents">
+            <Body headerBackground={headerBackground} />
           </div>
         </div>
       </div>
-      <div className="spotify_footer">
+      <div className="spotify__footer">
         <Footer />
       </div>
     </Container>
   );
-};
-
-export default Spotify;
+}
 
 const Container = styled.div`
   max-width: 100vw;
@@ -65,8 +77,7 @@ const Container = styled.div`
   overflow: hidden;
   display: grid;
   grid-template-rows: 85vh 15vh;
-
-  .spotify_body {
+  .spotify__body {
     display: grid;
     grid-template-columns: 15vw 85vw;
     height: 100%;
@@ -78,11 +89,12 @@ const Container = styled.div`
       width: 100%;
       overflow: auto;
       &::-webkit-scrollbar {
-      width: 0.7rem;
-      &-thumb {
-        background-color: rgba(255,255,255,0.6);
+        width: 0.7rem;
+        max-height: 2rem;
+        &-thumb {
+          background-color: rgba(255, 255, 255, 0.6);
+        }
       }
-    }
     }
   }
 `;
