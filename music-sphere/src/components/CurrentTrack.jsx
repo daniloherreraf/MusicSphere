4680b07e1f6289d96712d866f837cb703dece625
daniloherreraf <herreraf.danilo@gmail.com>
2023-06-07 16:09:04 -0500

    code-currentTrack

--- music-sphere/src/components/CurrentTrack.jsx
+++ music-sphere/src/components/CurrentTrack.jsx
@@ -1,69 +1,71 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import { useStateProvider } from '../utils/StateProvider';
-import axios from 'axios';
-import { reducerCases } from '../utils/Constants';
-
-const CurrentTrack = () => {
-  
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useStateProvider } from "../utils/StateProvider";
+import { reducerCases } from "../utils/Constants";
+export default function CurrentTrack() {
+  const [{ token, currentPlaying }, dispatch] = useStateProvider();
   useEffect(() => {
     const getCurrentTrack = async () => {
       const response = await axios.get(
-        'https://api.spotify.com/v1/me/player/currently-playing',
+        "https://api.spotify.com/v1/me/player/currently-playing",
         {
           headers: {
-            Authorization: 'Bearer ' + token,
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
           },
         }
       );
-      if (response.data !== '') {
-        const { item } = response.data;
-        const currentlyPlaying = {
-          id: item.id,
-          name: item.name,
-          artists: item.artists.map((artist) => artist.name),
-          image: item.album.images[2].url,
+      if (response.data !== "") {
+        const currentPlaying = {
+          id: response.data.item.id,
+          name: response.data.item.name,
+          artists: response.data.item.artists.map((artist) => artist.name),
+          image: response.data.item.album.images[2].url,
         };
-        dispatch({ type: reducerCases.SET_PLAYING, currentlyPlaying });
+        dispatch({ type: reducerCases.SET_PLAYING, currentPlaying });
+      } else {
+        dispatch({ type: reducerCases.SET_PLAYING, currentPlaying: null });
       }
     };
     getCurrentTrack();
   }, [token, dispatch]);
-
   return (
     <Container>
-      {currentlyPlaying && (
+      {currentPlaying && (
         <div className="track">
-          <div className="track_image">
-            <img src={currentlyPlaying.image} alt="currentlyPlaying" />
+          <div className="track__image">
+            <img src={currentPlaying.image} alt="currentPlaying" />
           </div>
-          <div className="track_info">
-            <h4>{currentlyPlaying.name}</h4>
-            <h6>{currentlyPlaying.artists.join(", ")}</h6>
+          <div className="track__info">
+            <h4 className="track__info__track__name">{currentPlaying.name}</h4>
+            <h6 className="track__info__track__artists">
+              {currentPlaying.artists.join(", ")}
+            </h6>
           </div>
         </div>
       )}
     </Container>
   );
-};
-
-export default CurrentTrack;
+}
 
 const Container = styled.div`
-.track {
+  .track {
     display: flex;
     align-items: center;
     gap: 1rem;
-    &_info {
-        display: flex;
-        flex-direction: column;
-        gap: 0.3rem;
-        h4 {
-            color: white;
-        }
-        h6 {
-            color: #b3b3b3;
-        }
+    &__image {
     }
-}`;
+    &__info {
+      display: flex;
+      flex-direction: column;
+      gap: 0.3rem;
+      &__track__name {
+        color: white;
+      }
+      &__track__artists {
+        color: #b3b3b3;
+      }
+    }
+  }
+`;
